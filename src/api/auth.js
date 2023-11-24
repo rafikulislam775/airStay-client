e1e7fdd6058ec02260024c21dcb4a2e2484d546d
rafikulislam775 <islamrafikul.jp@gmail.com>
2023-11-24 21:05:24 +0900

    added login page and remove tocken function

--- src/api/auth.js
+++ src/api/auth.js
@@ -4,9 +4,10 @@ import axiosSecure from '.'
 export const saveUser = async user => {
   const currentUser = {
     email: user.email,
-    role: 'guest',
-    status: 'Verified',
-  }
+    // role: 'admin',
+    role: "guest",
+    status: "Verified",
+  };
   const { data } = await axiosSecure.put(`/users/${user?.email}`, currentUser)
 
   return data
