7e93c39064c37aa52533c49650eba5d22ec36534
rafikulislam775 <islamrafikul.jp@gmail.com>
2023-11-24 19:14:46 +0900

    added sign up with user img upload by imm bb

---
+++ src/api/utils.js
@@ -0,0 +1,18 @@
+import axios from "axios";
+// import axiosSecure from ".";
+
+export const imageUpload = async (image) => {
+  const formData = new FormData();
+  formData.append("image", image);
+  const { data } = await axios.post(
+    `https://api.imgbb.com/1/upload?key=d206b620051696a0a87a5df768011c1b`,
+    formData
+  );
+  return data;
+};
+
+// admin stat
+// export const getAdminStat = async () => {
+//   const { data } = await axiosSecure(`/admin-stat`);
+//   return data;
+// };
--- src/components/RoomDetails/RoomReservation.jsx
+++ src/components/RoomDetails/RoomReservation.jsx
@@ -1,54 +1,54 @@
 /* eslint-disable react/prop-types */
-import { useState } from "react";
+// import { useState } from "react";
 import Button from "../Button/Button";
-import Calender from "./Calender";
-import { formatDistance } from "date-fns";
-import BookingModal from "../Modal/BookingModal";
-import useAuth from "../../hooks/useAuth";
+// import Calender from "./Calender";
+// import { formatDistance } from "date-fns";
+// import BookingModal from "../Modal/BookingModal";
+// import useAuth from "../../hooks/useAuth";
 
 const RoomReservation = ({ room }) => {
-  let [isOpen, setIsOpen] = useState(false);
-  const { user } = useAuth();
+  // let [isOpen, setIsOpen] = useState(false);
+  // const { user } = useAuth();
 
-  const closeModal = () => {
-    setIsOpen(false);
-  };
-  const [value, setValue] = useState({
-    startDate: new Date(room?.from),
-    endDate: new Date(room?.to),
-    key: "selection",
-  });
-  //   Total days * price
-  const totalDays = parseInt(
-    formatDistance(new Date(room?.to), new Date(room?.from)).split(" ")[0]
-  );
-  // Total Price Calculation
-  const totalPrice = totalDays * room?.price;
-  const handleDateChange = (ranges) => {
-    console.log(ranges);
-    setValue({
-      startDate: new Date(room?.from),
-      endDate: new Date(room?.to),
-      key: "selection",
-    });
-  };
-  const [bookingInfo, setBookingInfo] = useState({
-    guest: {
-      name: user?.displayName,
-      email: user?.email,
-      image: user?.photoURL,
-    },
-    host: room?.host?.email,
-    location: room?.location,
-    price: totalPrice,
-    to: value.endDate,
-    from: value.startDate,
-    title: room?.title,
-    roomId: room?._id,
-    image: room?.image,
-  });
+  // const closeModal = () => {
+  //   setIsOpen(false);
+  // };
+  // const [value, setValue] = useState({
+  //   startDate: new Date(room?.from),
+  //   endDate: new Date(room?.to),
+  //   key: "selection",
+  // });
+  // //   Total days * price
+  // const totalDays = parseInt(
+  //   formatDistance(new Date(room?.to), new Date(room?.from)).split(" ")[0]
+  // );
+  // // Total Price Calculation
+  // const totalPrice = totalDays * room?.price;
+  // const handleDateChange = (ranges) => {
+  //   console.log(ranges);
+  //   setValue({
+  //     startDate: new Date(room?.from),
+  //     endDate: new Date(room?.to),
+  //     key: "selection",
+  //   });
+  // };
+  // const [bookingInfo, setBookingInfo] = useState({
+  //   guest: {
+  //     name: user?.displayName,
+  //     email: user?.email,
+  //     image: user?.photoURL,
+  //   },
+  //   host: room?.host?.email,
+  //   location: room?.location,
+  //   price: totalPrice,
+  //   to: value.endDate,
+  //   from: value.startDate,
+  //   title: room?.title,
+  //   roomId: room?._id,
+  //   image: room?.image,
+  // });
 
-  console.log(value);
+  // console.log(value);
 
   return (
     <div className="rounded-xl border-[1px] border-neutral-200 overflow-hidden bg-white">
@@ -58,27 +58,27 @@ const RoomReservation = ({ room }) => {
       </div>
       <hr />
       <div className="flex justify-center">
-        <Calender handleDateChange={handleDateChange} value={value} />
+        {/* <Calender handleDateChange={handleDateChange} value={value} /> */}
       </div>
       <hr />
       <div className="p-4">
         <Button
           disabled={room.host.email === user.email || room.booked}
-          onClick={() => setIsOpen(true)}
+          // onClick={() => setIsOpen(true)}
           label={"Reserve"}
         />
       </div>
       <hr />
       <div className="p-4 flex items-center justify-between font-semibold text-lg">
         <div>Total</div>
-        <div>$ {totalPrice}</div>
+        {/* <div>$ {totalPrice}</div> */}
       </div>
 
-      <BookingModal
+      {/* <BookingModal
         closeModal={closeModal}
         isOpen={isOpen}
         bookingInfo={bookingInfo}
-      />
+      /> */}
     </div>
   );
 };
